614fe17f2c57dd58b529f8ee7df9211df59f68eb
Rustam <rustam905@gmail.com>
2021-02-23 14:40:44 -0800

    App creation started

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyB6pKsteHdhKtqZujSzLmJ6XHj5hBJ2SVs",
+	authDomain: "whatsappweb-30ebd.firebaseapp.com",
+	projectId: "whatsappweb-30ebd",
+	storageBucket: "whatsappweb-30ebd.appspot.com",
+	messagingSenderId: "496055914466",
+	appId: "1:496055914466:web:e33da9f659aac5111af3bb",
+	measurementId: "G-GCM2BXZYEB",
+};
