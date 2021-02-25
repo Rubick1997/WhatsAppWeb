73d00af90532200423b21a79f9358a6fb7de05ab
Rustam <rustam905@gmail.com>
2021-02-24 18:17:30 -0800

    Routing

--- src/Chat.js
+++ src/Chat.js
@@ -7,10 +7,22 @@ import InsertEmoticonRoundedIcon from "@material-ui/icons/InsertEmoticonRounded"
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 import MicIcon from "@material-ui/icons/Mic";
 import SendIcon from "@material-ui/icons/Send";
+import { useParams } from "react-router";
+import db from "./firebase";
 
 function Chat() {
 	const [seed, setSeed] = useState("");
 	const [message, setMessage] = useState("");
+	const { roomId } = useParams();
+	const [roomName, setRoomName] = useState("");
+
+	useEffect(() => {
+		if (roomId) {
+			db.collection("rooms")
+				.doc(roomId)
+				.onSnapshot((snapshot) => setRoomName(snapshot.data().name));
+		}
+	}, [roomId]);
 
 	useEffect(() => {
 		setSeed(Math.floor(Math.random() * 5000));
@@ -27,12 +39,12 @@ function Chat() {
 			<div className='chat__header'>
 				<Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
 				<div className='chat__headerInfo'>
-					<h3>Room name</h3>
+					<h3>{roomName}</h3>
 					<p>Last seen at ...</p>
 				</div>
 				<div className='chat__headerRight'>
 					<IconButton>
-						<SearchOutlined/>
+						<SearchOutlined />
 					</IconButton>
 					<IconButton>
 						<MoreVertIcon />
@@ -41,7 +53,7 @@ function Chat() {
 			</div>
 			<div className='chat__body'>
 				<p className={`chat__message ${true && "chat__reciever"}`}>
-					Hey
+					Hey, how are you
 					<p className='chat__timeStamp'>2:32pm</p>
 				</p>
 			</div>
